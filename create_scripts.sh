88d8e04f8e05ce1c1cb530d060403615c6cb2ade
psifive <simonquin@gmail.com>
2016-03-11 15:05:30 +0000

    added date functionality

--- create_scripts.sh
+++ create_scripts.sh
@@ -3,9 +3,18 @@
 URL=$1
 USERNAME=$2
 PASSWORD=$3
-DATENOW=$(date +%Y-%m-%dT%H:%M:%SZ)
 
-sed -e "s;%DATENOW%;${DATENOW};g" ./Templates/date.template > date.txt
+ENVIRONAME=$(uname)
+
+if [ ${ENVIRONAME} == 'Darwin' ]
+    then
+        DATENOW=$(date +%Y-%m-%dT%H:%M:%SZ)
+        sed -e "s;%DATENOW%;${DATENOW};g" ./Templates/date.template > date.txt
+    else
+        DATENOW=$(date /T)
+        sed -e "s;%DATENOW%;${DATENOW};g" ./Templates/date.template > date.txt
+fi
+
 sed -e "s;%URL%;${URL};g" ./Templates/url.template > url.txt
 sed -e "s;%URL%;${URL};g" -e "s;%USERNAME%;${USERNAME};g" -e "s;%PASSWORD%;${PASSWORD};g" ./Templates/get_token.template > get_token.sh
 
