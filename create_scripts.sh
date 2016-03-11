7fb964b826e5aba0fafb03ec095dd32fdfeea609
psifive <simonquin@gmail.com>
2016-03-11 14:14:05 +0000

    fixed typo

--- create_scripts.sh
+++ create_scripts.sh
@@ -5,7 +5,7 @@ USERNAME=$2
 PASSWORD=$3
 DATENOW=$(date +%Y-%m-%dT%H:%M:%SZ)
 
-sed -e "s;%DATENOW%";${DATENOW};g" ./Templates/datenow.template > date.txt
+sed -e "s;%DATENOW%;${DATENOW};g" ./Templates/date.template > date.txt
 sed -e "s;%URL%;${URL};g" ./Templates/url.template > url.txt
 sed -e "s;%URL%;${URL};g" -e "s;%USERNAME%;${USERNAME};g" -e "s;%PASSWORD%;${PASSWORD};g" ./Templates/get_token.template > get_token.sh
 
