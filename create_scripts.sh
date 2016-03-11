3694742f7b696a1790eaf69136b05935b0d937b3
Philip Taylor <philiptaylor@Jonathan-Smiths-Mac-mini.local>
2016-03-11 12:49:03 +0000

    minor updates

--- create_scripts.sh
+++ create_scripts.sh
@@ -3,7 +3,9 @@
 URL=$1
 USERNAME=$2
 PASSWORD=$3
+DATENOW=$(date +%Y-%m-%dT%H:%M:%SZ)
 
+sed -e "s;%DATENOW%";${DATENOW};g" ./Templates/datenow.template > date.txt
 sed -e "s;%URL%;${URL};g" ./Templates/url.template > url.txt
 sed -e "s;%URL%;${URL};g" -e "s;%USERNAME%;${USERNAME};g" -e "s;%PASSWORD%;${PASSWORD};g" ./Templates/get_token.template > get_token.sh
 
