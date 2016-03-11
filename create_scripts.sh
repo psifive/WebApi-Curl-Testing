7d6f04326770450acce01177a7a4be456c1c5c9b
psifive <simonquin@gmail.com>
2016-03-11 11:34:34 +0000

    updated to include chmod

--- create_scripts.sh
+++ create_scripts.sh
@@ -6,3 +6,5 @@ PASSWORD=$3
 
 sed -e "s;%URL%;${URL};g" ./Templates/url.template > url.txt
 sed -e "s;%URL%;${URL};g" -e "s;%USERNAME%;${USERNAME};g" -e "s;%PASSWORD%;${PASSWORD};g" ./Templates/get_token.template > get_token.sh
+
+chmod +x *
