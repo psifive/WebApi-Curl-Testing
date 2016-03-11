98c5e2460c5f794176274ebee01778fb02f699d9
Philip Taylor <philiptaylor@Jonathan-Smiths-Mac-mini.local>
2016-03-11 11:52:54 +0000

    mac-ify bash script

--- running_order.sh
+++ running_order.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-get_token.sh
-post_customer.sh
-post_customer_address.sh
-post_customer_bankaccount.sh
-post_application.sh
-put_application_bankaccountlink.sh
-put_application_status_accepted.sh
-get_application_documentrequests.sh
-get_application_documentrequest.sh
-post_application_signedcreditagreement.sh
-put_application_status_accepted.sh
+./get_token.sh
+./post_customer.sh
+./post_customer_address.sh
+./post_customer_bankaccount.sh
+./post_application.sh
+./put_application_bankaccountlink.sh
+./put_application_status_accepted.sh
+./get_application_documentrequests.sh
+./get_application_documentrequest.sh
+./post_application_signedcreditagreement.sh
+./put_application_status_accepted.sh
